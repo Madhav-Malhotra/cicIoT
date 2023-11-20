fd846fe37970abc297b34b22e1d07d23b66d7f3a
Madhav Malhotra <madhav.malhotra003@gmail.com>
2023-11-19 21:11:08 -0500

    Fixed SIGINT unresponsiveness in monitor script
    Problem:
    - SIGINT would be handled by sniff() function from scapy. Overall script would not stop as desired.
    
    Changes:
    - Created a custom handler for the SIGINT signal.
    
    Warning:
    - When adding future code to the script, ensure on_exit SIGINT handler function is updated.
    - Custom KeyboardError handling code may have been overwritten in scapy. Untested for effects.

--- iot_scripts/rp_monitor.py
+++ iot_scripts/rp_monitor.py
@@ -2,33 +2,33 @@
 
 import os
 import time
+import signal
 from scapy.all import sniff, wrpcap
-
-def packet_callback(packet):
-    packets.append(packet)
 
 def save_packets():
     filename = f"./network_traffic/{time.strftime('%Y%m%d%H%M%S')}.pcap"
     wrpcap(filename, packets)
-    print(f"Captured data saved to {filename}")
     del packets[:]
+
+    print(f"Captured data saved to {filename}")
 
 def monitor():
     while True:
-        try:
-            sniff(prn=packet_callback, store=False, timeout=10)
-        except KeyboardInterrupt:
-            break
+        sniff(prn=lambda x: packets.append(x), store=False, timeout=10)
         save_packets()
 
-if __name__ == "__main__":
-    packets = []
-    print("Starting packet monitoring")
+def on_exit(sig, frame):
+    save_packets()
+    print("Stopped packet monitoring")
+    exit(0)
 
-    # If ./network_traffic doesn't exist, create it
+if __name__ == "__main__":
+    # Setup data
+    packets = []
     os.makedirs("./network_traffic", exist_ok=True)
 
-    try:
-        monitor()
-    except KeyboardInterrupt:
-        print("Stopping packet monitoring.")
+    # Setup exit signal handler
+    signal.signal(signal.SIGINT, on_exit)
+
+    print("Starting packet monitoring")
+    monitor()
